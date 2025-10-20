638918bb2a775759e06a61862fac284b71e82a23
Satyam Gupta <18satyamgupta@gmail.com>
2025-10-20 14:13:12 +0530

    'data' folder updates
    'data' folder updated with train and test subfolders

---
+++ src/dataset.py
@@ -0,0 +1,30 @@
+import torch
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+
+def get_data_loaders(train_dir, test_dir, batch_size=32):
+    # Define image transformations
+    transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                             std=[0.229, 0.224, 0.225])
+    ])
+
+    # Load datasets
+    train_data = datasets.ImageFolder(root=train_dir, transform=transform)
+    test_data = datasets.ImageFolder(root=test_dir, transform=transform)
+
+    # Create data loaders
+    train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(test_data, batch_size=batch_size, shuffle=False)
+
+    class_names = train_data.classes
+
+    return train_loader, test_loader, class_names
+
+if __name__ == "__main__":
+    train_loader, test_loader, class_names = get_data_loaders("../data/train", "../data/test")
+    print("Classes:", class_names)
+    print("Train batches:", len(train_loader))
+    print("Test batches:", len(test_loader))
